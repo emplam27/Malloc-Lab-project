54057ce4903e942f2a1df765b3c1eb74d70a15af
emplam27 <emplam27@gmail.com>
2021-01-17 00:23:14 +0900

    Finish Implicit Malloc

--- 완성-implicit-malloc.c
+++ 완성-implicit-malloc.c
@@ -1,14 +1,3 @@
-/*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
- *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -18,30 +7,18 @@
 #include "mm.h"
 #include "memlib.h"
 
-/*********************************************************
- * NOTE TO STUDENTS: Before you do anything else, please
- * provide your team information in the following struct.
- ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "",
     /* First member's full name */
-    "Harry Bovik",
+    "Yong Uk",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
     ""
 };
-
-/* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
-
-/* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 #define WSIZE 4             // word, header, footer 사이즈(byte)
 #define DSIZE 8             // double word 사이즈(byte)
@@ -149,11 +126,6 @@ void mm_free(void *bp)
     size_t size = GET_SIZE(HDRP(bp));
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
-    // printf("freeeeeeeeeeeeeeeeeeeeeeeeee\n");
-    // printf("HDRP(bp): %p\n", HDRP(bp));
-    // printf("GET_ALLOC(HDRP(bp)): %d\n", GET_ALLOC(HDRP(bp)));
-    // printf("GET_SIZE(HDRP(bp)): %d\n", GET_SIZE(HDRP(bp)));
-    // printf("\n");
     coalesce(bp);
 }
 
@@ -183,22 +155,24 @@ static void *extend_heap(size_t words)
     char *bp;
     size_t size;
 
-    // words가 홀수여도 짝수로 만들어주기
+    /* 
+     * words가 홀수여도 짝수로 만들어주기
+     * 오류상황이 발생하면 프로그램을 종료
+     * 오류상황이 발생하지 않으면 block pointer는 새로 할당된 힙의 처음 block pointer
+     */
     size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
-    // 오류상황이 발생하면 프로그램을 종료
-    // 오류상황이 발생하지 않으면 block pointer는 새로 할당된 힙의 처음 block pointer
     if ((long)(bp = mem_sbrk(size)) == -1) {
         return NULL;
     }
 
-    // printf("extend1111111111111111111111\n");
-    // 처음 할당된 heap의 경우에는 alloc이 1인 block header와 block footer를 만들어 줬다면, 
-    // 새로 할당된 heap의 경우에는 alloc이 0인 block header와 block footer가 필요함
-    // 새로 할당된 heap에 free block header, free block footer, end point를 지정
+    /*
+     * 처음 할당된 heap의 경우에는 alloc이 1인 block header와 block footer를 만들어 줬다면, 
+     * 새로 할당된 heap의 경우에는 alloc이 0인 block header와 block footer가 필요함
+     * 새로 할당된 heap에 free block header, free block footer, end point를 지정
+     */
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
-    // printf("extend222222222222222222\n");
 
     // 만일 이전 블록이 free상태인 블록이라면 합쳐주기
     return coalesce(bp);
@@ -206,42 +180,17 @@ static void *extend_heap(size_t words)
 
 static void *coalesce(void* bp)
 {
-    // printf("start coalesce  eeeeeeeeeeeeeeeeeeee\n");
-    // printf("bp: %p\n", bp);
-    // printf("HDRP(bp): %p\n", HDRP(bp));
-    // printf("GET_ALLOC(HDRP(bp)): %d\n", GET_ALLOC(HDRP(bp)));
-    // printf("GET_SIZE(HDRP(bp)): %d\n", GET_SIZE(HDRP(bp)));
-    // printf("FTRP(bp): %p\n", FTRP(bp));
-    // printf("GET_ALLOC(FTRP(bp)): %d\n", GET_ALLOC(FTRP(bp)));
-    // printf("GET_SIZE(FTRP(bp)): %d\n", GET_SIZE(FTRP(bp)));
-    // printf("((char *)(bp) - DSIZE)): %p\n", ((char *)(bp) - DSIZE));
-    // printf("GET_SIZE(((char *)(bp) - DSIZE))): %d\n", GET_SIZE(((char *)(bp) - DSIZE)));
-    // printf("\n");
-    // printf("PREV_BLKP(bp): %p\n", PREV_BLKP(bp));
-    // printf("FTRP(PREV_BLKP(bp)): %p\n", FTRP(PREV_BLKP(bp)));
-    // printf("GET_ALLOC(FTRP(PREV_BLKP(bp))): %d\n", GET_ALLOC(FTRP(PREV_BLKP(bp))));
-    // printf("GET_SIZE(FTRP(PREV_BLKP(bp))): %d\n", GET_SIZE(FTRP(PREV_BLKP(bp))));
-    // printf("HDRP(PREV_BLKP(bp)): %p\n", HDRP(PREV_BLKP(bp)));
-    // printf("GET_ALLOC(HDRP(PREV_BLKP(bp))): %d\n", GET_ALLOC(HDRP(PREV_BLKP(bp))));
-    // printf("GET_ALLOC(HDRP(PREV_BLKP(bp))): %d\n", GET_ALLOC(HDRP(PREV_BLKP(bp))));
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    // printf("prev_alloc: %d\n", prev_alloc);
-    // printf("middle1111111 coalesce  eeeeeeeeeeeeeeeeeeee\n");
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    // printf("middle22222222 coalesce  eeeeeeeeeeeeeeeeeeee\n");
     size_t size = GET_SIZE(HDRP(bp));
-    // printf("middle33333333333 coalesce  eeeeeeeeeeeeeeeeeeee\n");
 
     // free한 block 앞, 뒤에 모두 할당 되어있는 block이 있는 경우
     if (prev_alloc && next_alloc) {
-        // printf("coalesce  1111111111111111111\n");
         return bp;
     }
 
     // free한 블록 뒤에만 free 되어있는 block이 있는 경우
     else if (prev_alloc && !next_alloc) {
-        // printf("coalesce  2222222222222222222\n");
-
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
@@ -249,8 +198,6 @@ static void *coalesce(void* bp)
 
     // free한 블록 앞에만 free 되어있는 block이 있는 경우
     else if (!prev_alloc && next_alloc) {
-        // printf("coalesce  333333333333333333333\n");
-
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
@@ -259,14 +206,11 @@ static void *coalesce(void* bp)
 
     // free한 block 앞, 뒤에 모두 free 되어있는 block이 있는 경우
     else {
-        // printf("coalesce  444444444444444444444\n");
-
         size += (GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp))));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
-    // printf("end coalesce  eeeeeeeeeeeeeeeeeeee\n");
     return bp;
 }
 
@@ -275,31 +219,12 @@ static void* find_fit(size_t asize)
     size_t curr_alloc;
     size_t curr_size;
     void* curr_bp = heap_listp;
-
-    // printf("-------------beforefind-------------\n");
-    // printf("bp : %p\n", curr_bp);
-    // printf("asize : %d\n", asize);
-    // printf("GET_ALLOC(HDRP(curr_bp) : %d\n", GET_ALLOC(HDRP(curr_bp)));
-    // printf("GET_SIZE(HDRP(bp)) : %d\n", GET_SIZE(HDRP(curr_bp)));
-    // printf("\n");
-
     while (1) {
-        // printf("-------------find-------------\n");
-        // printf("bp : %p\n", curr_bp);
-        // printf("asize : %d\n", asize);
-        // printf("HDRP(curr_bp) : %p\n", HDRP(curr_bp));
-        // printf("GET_ALLOC(HDRP(curr_bp) : %d\n", GET_ALLOC(HDRP(curr_bp)));
-        // printf("GET_SIZE(HDRP(bp)) : %d\n", GET_SIZE(HDRP(curr_bp)));
-        // printf("FTRP(curr_bp) : %p\n", FTRP(curr_bp));
-        // printf("GET_ALLOC(FTRP(curr_bp)) : %d\n", GET_ALLOC(FTRP(curr_bp)));
-        // printf("GET_SIZE(FTRP(curr_bp)) : %d\n", GET_SIZE(FTRP(curr_bp)));
-        // printf("\n");
         curr_alloc = GET_ALLOC(HDRP(curr_bp));
         curr_size = GET_SIZE(HDRP(curr_bp));
 
         // end point에 도달하게 되면 NULL 반환
         if (!curr_size) {
-            // printf("find endpoint\n");
             return NULL;
         }
 
@@ -314,30 +239,16 @@ static void* find_fit(size_t asize)
             continue;
         }
         // 현재 포인터의 block이 할당되어있지 않고, 사이즈가 asize보다 크다면 현재 위치 반환
-        // printf("find fit success\n");
         return curr_bp;
     }
-    // printf("find end???????????????\n");
 }
 
 static void place(void* bp, size_t asize) 
 {
-    // printf("-------------place-------------\n");
-    // printf("bp : %p\n", bp);
-    // printf("asize : %d\n", asize);
-    // printf("GET_SIZE(HDRP(bp)) : %d\n", GET_SIZE(HDRP(bp)));
-
     // 만일 할당 하려는 사이즈와 공간의 사이즈가 같다면 header, footer의 alloc만 변환
     if (GET_SIZE(HDRP(bp)) == asize) {
         PUT(HDRP(bp), PACK(asize, 1));
         PUT(FTRP(bp), PACK(asize, 1));
-        // printf("header : %p\n", HDRP(bp));
-        // printf("GET_ALLOC(HDRP(bp)) : %d\n", GET_ALLOC(HDRP(bp)));
-        // printf("GET_SIZE(HDRP(bp)) : %d\n", GET_SIZE(HDRP(bp)));
-        // printf("footer : %p\n", FTRP(bp));
-        // printf("GET_ALLOC(FTRP(bp)) : %d\n", GET_ALLOC(FTRP(bp)));
-        // printf("GET_SIZE(FTRP(bp)) : %d\n", GET_SIZE(FTRP(bp)));
-        // printf("\n");
     }
 
     /* 
@@ -348,8 +259,6 @@ static void place(void* bp, size_t asize)
      * FTRP(old_bp)에 old_size - asize, alloc 0 설정
      */
     else {
-        // printf("place unsame size\n");
-        // printf("\n");
         void *old_bp, *new_bp;
         size_t old_size, remain_size;
 
@@ -368,21 +277,6 @@ static void place(void* bp, size_t asize)
         void* new_footer = FTRP(old_bp);
         PUT(new_header, PACK(remain_size, 0)); // new_header
         PUT(new_footer, PACK(asize, 1)); // new_footer
-
-        // printf("old_header : %p\n", old_header);
-        // printf("GET_ALLOC(old_header) : %d\n", GET_ALLOC(old_header));
-        // printf("GET_SIZE(old_header) : %d\n", GET_SIZE(old_header));
-        // printf("old_footer : %p\n", old_footer);
-        // printf("GET_ALLOC(old_footer) : %d\n", GET_ALLOC(old_footer));
-        // printf("GET_SIZE(old_footer) : %d\n", GET_SIZE(old_footer));
-        // printf("new_header : %p\n", new_header);
-        // printf("GET_ALLOC(new_header) : %d\n", GET_ALLOC(new_header));
-        // printf("GET_SIZE(new_header) : %d\n", GET_SIZE(new_header));
-        // printf("new_footer : %p\n", new_footer);
-        // printf("GET_ALLOC(new_footer) : %d\n", GET_ALLOC(new_footer));
-        // printf("GET_SIZE(new_footer) : %d\n", GET_SIZE(new_footer));
-        // printf("\n");
-
     }
 }
 
